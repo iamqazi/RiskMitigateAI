f7c36420766a7781bd6e583e5f8f7cb61de6e3a0
iamqazi <qaziobaid7897@gmail.com>
2025-01-10 14:34:01 +0500

    changes

--- components/banner/index.tsx
+++ components/banner/index.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
     router.push("/signup");
   };
   return (
-    <section className="max-w-[1181px]  lg:mt-2  mx-auto pb-[60px] bg-black px-4 md:px-6">
+    <section className="max-w-[1181px]  mt-12  mx-auto pb-[60px] bg-black px-4 md:px-6">
       <div className="bg-[#01F2A7] px-6 md:px-8 rounded-[24px] h-auto md:h-[260px] flex flex-col md:flex-row items-center justify-between gap-6 md:gap-0">
         <div className="flex flex-col gap-4 md:gap-6 px-4 md:px-6 text-center md:text-left">
           <h1 className="text-[32px] md:text-[48px] font-medium font-heading text-black">
--- components/benefits/index.tsx
+++ components/benefits/index.tsx
@@ -11,6 +11,31 @@ const Benefits: React.FC = () => {
   return (
     <section className="max-w-[1181px] mx-auto py-16 sm:py-20 md:py-24 px-4">
       <div className="flex flex-col md:flex-row justify-between gap-8 md:gap-[60px]">
+        {" "}
+        {/* Right div with button, heading, and para */}
+        <div className="flex flex-col justify-center items-start gap-6">
+          <button className="w-[120px] sm:w-[134px] h-[36px] border px-2 border-[#464646] font-medium bg-gradient-to-b from-[#1F1F23] to-[#2E2D35] text-white rounded-[8px]">
+            <span className="flex gap-2 items-center">
+              <Image src={"/benefit.png"} height={28} width={28} alt="img" />
+              <span className="uppercase font-text">Benefits</span>
+            </span>
+          </button>
+
+          <h2 className="text-[28px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-heading font-normal text-white text-center sm:text-left sm:px-0">
+            Why Choose RiskMitigateAI?
+          </h2>
+
+          <p className="text-[16px] sm:text-[18px] md:text-[20px] font-text text-[#C5C6C5]">
+            Discover the key benefits that set us apart in delivering smarter
+            risk management solutions for lending platforms.
+          </p>
+          <button
+            onClick={handleButtonClick}
+            className="px-4 sm:px-6 py-2 sm:py-3 text-[14px] sm:text-[16px] w-[140px] sm:w-[152px] h-[40px] sm:h-[45px] bg-[#01F2A7] text-black rounded-[8px] font-semibold"
+          >
+            Get Started
+          </button>
+        </div>
         {/* Left div with three sub-divs */}
         <div className="flex flex-col gap-6 md:gap-8">
           <div className="flex gap-4 hover:bg-[#100F1480] rounded-[8px] hover:border-[#29282D] border border-transparent p-4 transition-all duration-300">
@@ -58,31 +83,6 @@ const Benefits: React.FC = () => {
               </p>
             </div>
           </div>
-        </div>
-
-        {/* Right div with button, heading, and para */}
-        <div className="flex flex-col justify-center items-start gap-6">
-          <button className="w-[120px] sm:w-[134px] h-[36px] border px-2 border-[#464646] font-medium bg-gradient-to-b from-[#1F1F23] to-[#2E2D35] text-white rounded-[8px]">
-            <span className="flex gap-2 items-center">
-              <Image src={"/benefit.png"} height={28} width={28} alt="img" />
-              <span className="uppercase font-text">Benefits</span>
-            </span>
-          </button>
-
-          <h2 className="text-[28px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-heading font-normal text-white text-center sm:text-left sm:px-0">
-            Why Choose RiskMitigateAI?
-          </h2>
-
-          <p className="text-[16px] sm:text-[18px] md:text-[20px] font-text text-[#C5C6C5]">
-            Discover the key benefits that set us apart in delivering smarter
-            risk management solutions for lending platforms.
-          </p>
-          <button
-            onClick={handleButtonClick}
-            className="px-4 sm:px-6 py-2 sm:py-3 text-[14px] sm:text-[16px] w-[140px] sm:w-[152px] h-[40px] sm:h-[45px] bg-[#01F2A7] text-black rounded-[8px] font-semibold"
-          >
-            Get Started
-          </button>
         </div>
       </div>
     </section>
